d0e7c99699082b09bea1cbd13fa7290d27a875fb
Sinitame <sinitame@protonmail.com>
2022-12-01 08:00:29 +0100

    Day1: part 2

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -2,21 +2,21 @@ use std::{fs, path::PathBuf};
 
 fn main() {
     let input = fs::read_to_string(PathBuf::from("input.txt")).unwrap();
-    let calories = input.lines().fold(vec![0], |mut calories, line| {
+    let mut calories = input.lines().fold(vec![(1, 0)], |mut calories, line| {
         if let Ok(calory) = line.parse::<usize>() {
             let num_processed = calories.len();
             if num_processed > 0 {
-                calories.get_mut(num_processed - 1).map(|it| *it += calory);
+                calories.get_mut(num_processed - 1).map(|it| it.1 += calory);
             }
         } else {
-            calories.push(0)
+            calories.push((calories.len(), 0))
         }
         calories
     });
-    let elf_with_max_cal = calories
-        .iter()
-        .enumerate()
-        .max_by(|x, y| x.1.cmp(y.1))
-        .unwrap();
-    println!("{}", elf_with_max_cal.1);
+
+    calories.sort_by(|x, y| x.1.cmp(&y.1));
+    println!(
+        "{}",
+        calories.iter().rev().take(3).map(|it| it.1).sum::<usize>()
+    );
 }
