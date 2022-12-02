b399417aa88720f3f566b0588faa4d0cd9d89776
Sinitame <sinitame@protonmail.com>
2022-12-02 09:28:06 +0100

    Partial solutions, works only for test

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,8 +1,8 @@
 use std::fs;
 
 fn main() {
-    let input = fs::read_to_string("input.txt").unwrap();
-    let score = input
+    let input = fs::read_to_string("test.txt").unwrap();
+    let score_1 = input
         .lines()
         .map(|it| {
             let vals = it.chars().map(|it| it as usize).collect::<Vec<_>>();
@@ -18,5 +18,21 @@ fn main() {
             round_score as usize
         })
         .sum::<usize>();
-    println!("{:?}", score);
+    let score_2 = input
+        .lines()
+        .map(|it| {
+            let vals = it.chars().map(|it| it as usize).collect::<Vec<_>>();
+            let strategy = vals[2] - 88;
+            let oponent_choice = vals[0] - 64;
+            if strategy == 0 {
+                (oponent_choice + 1) % 3 + 1
+            } else if strategy == 1 {
+                3 + oponent_choice
+            } else {
+                (oponent_choice + 1) % 3 + 6
+            }
+        })
+        .sum::<usize>();
+    println!("{:?}", score_1);
+    println!("{:?}", score_2);
 }
